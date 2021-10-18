b87780fe49dc3b1a3f8a62270989aa012c4434bf
nilesh44 <nileshkumarjaiswal7@gmail.com>
2021-10-18 12:52:05 +0530

    remove commented code

--- src/main/java/com/ace/config/ApplicationBean.java
+++ src/main/java/com/ace/config/ApplicationBean.java
@@ -7,21 +7,6 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class ApplicationBean {
 
-	/*
-	 * //here we can set the order of creation of bean
-	 * 
-	 * @Order(value = 1) //here we indicate to spring context create spring bean
-	 * with bean name "DbProperties" //@Bean("dbProperties") //here we can define
-	 * scope of bean
-	 * 
-	 * @Scope("singleton")
-	 * 
-	 * public DbProperties getDbProperties() { return new DbProperties(); }
-	 */
-
-	// here we can set the order of creation of bean
-	// @Order(value = 1)
-
 	@Scope("singleton")
 	// this annotation describe that create this bean only after depends on bean is
 	// created
